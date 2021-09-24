76b5e60213352ef49be0ced05014aeba56e3a9d2
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-24 17:27:10 +0530

    Day-01 complete

--- 03-functions/demo-10.go
+++ 03-functions/demo-10.go
@@ -10,14 +10,27 @@ func main() {
 }
 
 func f1() {
-	defer fmt.Println("Deferred from [@f1] - 1")
+	/* defer fmt.Println("Deferred from [@f1] - 1")
 	defer fmt.Println("Deferred from [@f1] - 2")
-	defer fmt.Println("Deferred from [@f1] - 3")
+	defer fmt.Println("Deferred from [@f1] - 3") */
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("recovering from ", err)
+		} else {
+			fmt.Println("f1 completed successfully!")
+		}
+	}()
+	defer func() {
+		fmt.Println("Deferred from [@f1] - 1")
+		fmt.Println("Deferred from [@f1] - 2")
+		fmt.Println("Deferred from [@f1] - 3")
+	}()
 	f2()
 	fmt.Println("f1 completed")
 }
 
 func f2() {
+
 	defer fmt.Println("Deferred from [@f2] - 1")
 	defer fmt.Println("Deferred from [@f2] - 2")
 	defer fmt.Println("Deferred from [@f2] - 3")
