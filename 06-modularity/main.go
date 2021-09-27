03c345144a112cbf92e2b1702afe6162dc894dd0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-27 14:32:23 +0530

    modularity demo

--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	calc "modularity-demo/calculator" /* package alias */
 	"modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println(calc.Add(10, 20))
-	fmt.Println(calc.Subtract(10, 20))
-	fmt.Println(utils.IsEven(10))
-	fmt.Println("Op Count = ", calc.GetOpCount())
+	log := logrus.New()
+	color.Green(fmt.Sprintf("Add result = %d\n", calc.Add(10, 20)))
+	color.Red(fmt.Sprintf("Subtract result = %d\n", calc.Subtract(10, 20)))
+
+	log.Println(utils.IsEven(10))
+	log.Println("Op Count = ", calc.GetOpCount())
 }
