82fdbc6c34049ae509560edfd1be3ab0624e423d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-28 16:54:04 +0530

    assignment solution

--- 10-concurrency/demo-11.go
+++ 10-concurrency/demo-11.go
@@ -9,15 +9,19 @@ import (
 the "hello" & "world" should be printed alternatively
 */
 func main() {
-	go print("hello")
-	go print("world")
+	ch1, ch2 := make(chan string), make(chan string)
+	go print("hello", ch1, ch2)
+	go print("world", ch2, ch1)
+	ch1 <- "start"
 	var input string
 	fmt.Scanln(&input)
 }
 
-func print(s string) {
+func print(s string, in, out chan string) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(time.Second * 2)
 		println(s)
+		out <- "done"
 	}
 }
