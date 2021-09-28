1ad8361a1da7e27e02ec61b15e1f61b190699efa
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-28 16:02:09 +0530

    using channels

---
+++ 10-concurrency/demo-08.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg *sync.WaitGroup = &sync.WaitGroup{}
+
+func main() {
+	wg.Add(1)
+	ch := make(chan int)
+	go add(100, 200, ch)
+	wg.Wait()
+	result := <-ch
+	fmt.Println(result)
+	fmt.Println("End of main")
+}
+
+func add(x, y int, ch chan int) {
+	result := x + y
+	ch <- result
+	wg.Done()
+}
