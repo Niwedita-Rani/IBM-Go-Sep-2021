b4f87a3fbd0d658ef2237d28e8a7fcdb3ca55135
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-28 10:31:49 +0530

    assignment

--- 09-interfaces/program.go
+++ 09-interfaces/program.go
@@ -14,6 +14,10 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
 //Sprint-2
 type Rectangle struct {
 	Height float64
@@ -22,6 +26,10 @@ type Rectangle struct {
 
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
 }
 
 //Sprint-3
@@ -34,22 +42,36 @@ func PrintArea(shape ShapeWithArea) {
 }
 
 //Sprint-4
-func PrintPerimeter( /*  */ ) {
-	fmt.Println("Perimeter = ", c.Perimeter())
+
+type ShapeWithPerimeter interface {
+	Perimeter() float64
 }
 
-/*
-implement whatever is needed
-*/
+func PrintPerimeter(shape ShapeWithPerimeter) {
+	fmt.Println("Perimeter = ", shape.Perimeter())
+}
+
+//Sprint-5
+func PrintDimensions( /*  */ ) {
+	//Print Area
+	//Print Perimeter
+
+}
 
 func main() {
 	c := Circle{Radius: 5}
 	//fmt.Println("Area = ", c.Area())
-	PrintArea(c)
-	PrintPerimeter(c)
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintDimensions(c)
 
 	r := Rectangle{Height: 5, Width: 10}
 	//fmt.Println("Area = ", r.Area())
-	PrintArea(r)
-	PrintPerimeter(r)
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintDimensions(r)
 }
